4102a6669de94f7317628aaa1c2d72d85aeae309
caijun <caijun@atoml.com>
2025-04-02 16:38:09 +0800

    minio reader 跑通

--- plugin/doc-reader/tools/minio-reader.py
+++ plugin/doc-reader/tools/minio-reader.py
@@ -10,13 +10,43 @@ from llama_index.readers.minio import (
 
 from pydantic import BaseModel
 class ToolParameters(BaseModel):
-    samples: str
+    endpoint_url: str
+    bucket: str
+    minio_access_key: str
+    minio_secret_key: str
+    minio_secure: bool
+    key: str
+
+mime_type_map = {
+    "PDF": "application/pdf",
+    "JSON": "application/json",
+    "MD": "text/markdown",
+    "TXT": "text/plain",
+}
 
 class MinioReaderTool(Tool):
     def _invoke(self, tool_parameters: dict[str, Any]) -> Generator[ToolInvokeMessage]:
-        if tool_parameters.get("samples") is None:
-            raise ValueError("samples is required")
-        
         params = ToolParameters(**tool_parameters)
-        samples = params.samples
+                
+        reader = MinioReader(
+            minio_endpoint=params.endpoint_url,
+            bucket=params.bucket,
+            minio_access_key=params.minio_access_key,
+            minio_secret_key=params.minio_secret_key,
+            minio_secure = params.minio_secure,
+            key = params.key
+        )    # Read the file
+        
+        documents = reader.load_data()
 
+        result = [
+            {"content": doc.text, "metadata": doc.metadata} for doc in documents
+        ]
+
+        if params.key is None or params.key == "":
+            # If no key is provided, return all documents
+            yield self.create_json_message({"documents": result})
+        else:
+            yield self.create_json_message({params.key: result})
+
+        
